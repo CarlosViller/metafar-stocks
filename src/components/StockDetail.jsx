7c7be326cd9dc56cc8faaa7611b57201cf7cdff4
Deltaaaaq <carlos.viller98@gmail.com>
2023-09-23 14:39:18 -0300

    use chart controls on stock details page

--- src/components/StockDetail.jsx
+++ src/components/StockDetail.jsx
@@ -1,13 +1,22 @@
-import { CircularProgress, useTheme } from "@mui/material";
+import { CircularProgress, FormControl, MenuItem, Select } from "@mui/material";
+import dayjs from "dayjs";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import StockDetailHeader from "./StockDetailHeader";
+import StockDetailDatePicker from "./StockDetailDatePicker";
+import ChartTypeSelection from "./ChartTypeSelection";
 
 export default function StockDetail() {
   const [stock, setStock] = useState();
   const [loading, setLoading] = useState(true);
-  const { symbol } = useParams();
-  const theme = useTheme();
+  const [chartType, setChartType] = useState("tiempo real");
+  const [interval, setInterval] = useState("5m");
+  const [historicDates, setHistoricDates] = useState({
+    from: dayjs(),
+    to: dayjs(),
+  });
 
+  const { symbol } = useParams();
   useEffect(() => {
     fetch(
       `https://api.twelvedata.com/stocks?symbol=${symbol}&source=docs&country=US`
@@ -25,13 +34,28 @@ export default function StockDetail() {
     );
 
   return (
-    <section>
-      <section className="stock-detail-header">
-        <h1 style={{ color: theme.palette.primary.main, fontSize: "3em" }}>
-          {stock.symbol}
-        </h1>
-        <small>{stock.name}</small>
-      </section>
+    <section id="stock-detail-container">
+      <StockDetailHeader stock={stock} />
+      <FormControl>
+        <ChartTypeSelection chartType={chartType} setChartType={setChartType} />
+        {chartType === "historico" && (
+          <StockDetailDatePicker
+            historicDates={historicDates}
+            setHistoricDates={setHistoricDates}
+          />
+        )}
+
+        <Select
+          labelId="interval-label"
+          value={interval}
+          onChange={(e) => setInterval(e.target.value)}
+        >
+          <MenuItem value="1m">1m</MenuItem>
+          <MenuItem value="5m">5m</MenuItem>
+          <MenuItem value="15m">15m</MenuItem>
+        </Select>
+      </FormControl>
+      <section className="stock-graph-container"></section>
     </section>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { ThemeProvider, createTheme } from "@mui/material";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
 const theme = createTheme({
   palette: {
@@ -11,8 +13,10 @@ const theme = createTheme({
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </LocalizationProvider>
   </React.StrictMode>
 );
