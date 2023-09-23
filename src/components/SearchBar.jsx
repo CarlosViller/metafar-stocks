b291a5fc27c18d6cd5f5a243edc092d18cd98dc4
Deltaaaaq <carlos.viller98@gmail.com>
2023-09-23 11:25:58 -0300

    push to /search

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,13 +3,21 @@ import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function SearchBar() {
+  const [type, setType] = useState("symbol");
+  const [query, setQuery] = useState("");
+
   return (
     <div id="search-container">
       <section id="search-bar">
-        <input type="text" />
-        <Link to="/">
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <Link to={`/search?type=${type}&q=${query}`}>
           <img width={25} src="/search-icon.svg" alt="search icon" />
         </Link>
       </section>
@@ -18,20 +26,9 @@ export default function SearchBar() {
           row
           aria-labelledby="demo-row-radio-buttons-group-label"
           name="row-radio-buttons-group"
+          value={type}
+          onChange={(e) => setType(e.target.value)}
         >
-          <FormControlLabel
-            value="name"
-            control={
-              <Radio
-                sx={{
-                  "&.Mui-checked": {
-                    color: "#ad4de8",
-                  },
-                }}
-              />
-            }
-            label="Name"
-          />
           <FormControlLabel
             value="symbol"
             control={
@@ -44,6 +41,19 @@ export default function SearchBar() {
               />
             }
             label="Symbol"
+          />
+          <FormControlLabel
+            value="name"
+            control={
+              <Radio
+                sx={{
+                  "&.Mui-checked": {
+                    color: "#ad4de8",
+                  },
+                }}
+              />
+            }
+            label="Name"
           />
         </RadioGroup>
       </FormControl>
