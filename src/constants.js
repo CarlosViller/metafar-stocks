aa71f136293777fd8c98ec5cd03d3429762f0b52
Deltaaaaq <carlos.viller98@gmail.com>
2023-09-27 12:11:22 -0300

    extract constants

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+export const CHART_TYPES = {
+  historico: "historico",
+  tiempo_real: "tiempo real"
+}
+
+// Una buena practica seria guardar la key en un archivo .env, ya que es un challange, la dejo aca por practicidad.
+export const API_KEY = "2cf36da53dc14a07860fd406140fba56";
+export const API_ENDPOINT = "https://api.twelvedata.com";
+
+export const TIME_INTERVALS = {
+  "1min": "1min",
+  "5min": "5min",
+  "15min": "15min",
+}
